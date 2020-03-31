03ee33acf42ccebc08db3d888f77f695faede546
Tom Nelson <tnelson@legacy.com>
2020-03-31 09:11:57 -0500

    #comment Completed work on async and promises

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,16 +1,49 @@
-import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
+import setText, { appendText, showWaiting, hideWaiting } from "./results.mjs";
 
+// When a prmoise goes from pending to fulfilled, it is
+// handled by a call to .then() functions
 export function get() {
+    axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+        setText(JSON.stringify(data));
+    });
 }
 
 export function getCatch() {
+    axios.get('http://localhost:3000/orders/123').then(({ data }) => {
+        setText(JSON.stringify(result.data));
+    }).catch(err => setText(err));
 }
 
 export function chain() {
+    axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    }).then(({ data }) => {
+        setText(`City: ${data.city}`);
+    })
 }
 
 export function chainCatch() {
+    axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    }).catch(err => {
+        setText(err);
+        return { data: {} };
+    }).then(({ data }) => {
+        setText(`City: ${data.city}`);
+    }).catch(err => setText(err));
 }
 
 export function final() {
+    showWaiting();
+    axios.get('http://localhost:3000/orders/1').then(({ data }) => {
+        return axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    }).then(({ data }) => {
+        setText(`City: ${data.city}`);
+    }).catch(err => setText(err))
+        .finally(() => {
+            setTimeout(() => {
+                hideWaiting();
+            }, 1500);
+            appendText(" -- Completely Done");
+        })
 }
